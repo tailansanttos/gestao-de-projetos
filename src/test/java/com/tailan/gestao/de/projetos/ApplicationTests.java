18d2555dfdefeafbbae05eb408ce55d21aa4d7ed
tailan <tailansanttos02@gmail.com>
2025-10-14 23:03:53 -0300

    a

---
+++ src/test/java/com/tailan/gestao/de/projetos/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tailan.gestao.de.projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
