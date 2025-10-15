52fd978e0f6a569770ea320a1315e218c5719180
tailan <tailansanttos02@gmail.com>
2025-10-15 18:03:29 -0300

    feat: service de user implementado e mapper criado.

---
+++ src/main/java/com/tailan/gestao/de/projetos/application/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.tailan.gestao.de.projetos.application.mapper;
+
+import com.tailan.gestao.de.projetos.application.dto.user.CreateUserDTO;
+import com.tailan.gestao.de.projetos.application.dto.user.UserResponseDTO;
+import com.tailan.gestao.de.projetos.core.model.user.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserResponseDTO toResponse(User user) {
+        return new UserResponseDTO(user.getName(), user.getEmail(), user.getCreatedAt());
+    }
+
+    public User toEntity(CreateUserDTO createUserDTO) {
+        return new User(createUserDTO.name(), createUserDTO.email(), createUserDTO.password());
+    }
+}
---
+++ src/main/java/com/tailan/gestao/de/projetos/application/service/user/impl/UserServiceImpl.java
@@ -0,0 +1,97 @@
+package com.tailan.gestao.de.projetos.application.service.user.impl;
+
+import com.tailan.gestao.de.projetos.application.dto.user.CreateUserDTO;
+import com.tailan.gestao.de.projetos.application.dto.user.UpdatePasswordDTO;
+import com.tailan.gestao.de.projetos.application.dto.user.UpdateUserDTO;
+import com.tailan.gestao.de.projetos.application.dto.user.UserResponseDTO;
+import com.tailan.gestao.de.projetos.application.mapper.UserMapper;
+import com.tailan.gestao.de.projetos.application.service.user.UserService;
+import com.tailan.gestao.de.projetos.core.model.user.User;
+import com.tailan.gestao.de.projetos.core.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public UserResponseDTO createUser(CreateUserDTO dto) {
+        Optional<User> existingUser = userRepository.findByEmail(dto.email());
+        if (existingUser.isPresent()) {
+            throw new IllegalArgumentException("Usuário com esse email já cadastrado. Tente com outro email.");
+        }
+
+        User user = userMapper.toEntity(dto);
+        user.setPasswordHash(passwordEncoder.encode(dto.password()));
+        User savedUser = userRepository.save(user);
+        return userMapper.toResponse(savedUser);
+    }
+
+    @Override
+    public UserResponseDTO updateUser(UUID userId, UpdateUserDTO dto) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado."));
+
+        if (dto.email() != null || !dto.email().isEmpty()) {
+            Optional<User> existingUserWithEmail = userRepository.findByEmail(dto.email());
+            if (existingUserWithEmail.isPresent() && !existingUserWithEmail.get().getId().equals(user.getId())) {
+                throw new IllegalArgumentException("Email já está sendo usado por outro usuário.");
+            }
+            user.setEmail(dto.email());
+        }
+
+        if (dto.name() != null || !dto.name().isEmpty()) {
+            user.setName(dto.name());
+        }
+
+        User updateUser = userRepository.save(user);
+        return userMapper.toResponse(updateUser);
+    }
+
+    @Override
+    public void updatePassword(UUID userId, UpdatePasswordDTO dto) {
+        //VERIFICAR SE PASSWORD ENCODER MATCHES SENHA ANTIGA COM A SENHA DO USER SALVA HASHEADA. VERIFICA SE A SENHA CRIPTOGRAFADA BATE COM A ANTIGA DO USUARIO
+        User user = getUserById(userId);
+
+        if (!passwordEncoder.matches(dto.oldPassword(), user.getPasswordHash())){
+            throw new IllegalArgumentException("Senha atual incorreta.");
+        }
+        user.setPasswordHash(passwordEncoder.encode(dto.newPassword()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(UUID userId) {
+       User user = userRepository.findById(userId)
+               .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado."));
+       userRepository.delete(user);
+    }
+
+    @Override
+    public User getUserById(UUID userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado."));
+
+    }
+
+    @Override
+    public List<UserResponseDTO> listAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(userMapper::toResponse).collect(Collectors.toList());
+    }
+}
