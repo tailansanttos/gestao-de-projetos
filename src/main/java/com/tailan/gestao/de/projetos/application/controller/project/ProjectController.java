d7ffec34aba699fabf0600d196e3fe86f46faae5
tailan <tailansanttos02@gmail.com>
2025-10-17 11:13:41 -0300

    feat: controller de project criado e implementado com todos os projetos.

---
+++ src/main/java/com/tailan/gestao/de/projetos/application/controller/project/ProjectController.java
@@ -0,0 +1,70 @@
+package com.tailan.gestao.de.projetos.application.controller.project;
+
+import com.tailan.gestao.de.projetos.application.dto.member.ProjectMemberDTO;
+import com.tailan.gestao.de.projetos.application.dto.project.*;
+import com.tailan.gestao.de.projetos.application.service.project.ProjectService;
+import jakarta.persistence.GeneratedValue;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/project")
+public class ProjectController {
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+    @PostMapping("/create")
+    public ResponseEntity<ProjectResponseDTO> createProject(@RequestBody CreateProjectDTO createProjectDTO) {
+        ProjectResponseDTO projectResponseDTO = projectService.createProject(createProjectDTO);
+        return new ResponseEntity<>(projectResponseDTO, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/addMember")
+    public ResponseEntity<Void> addMemberToProject(@RequestBody AddMemberDTO addMemberDTO) {
+        projectService.addMemberToProject(addMemberDTO);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/removeMember")
+    public ResponseEntity<Void> removeMemberFromProject(@RequestBody RemoveMemberDTO removeMemberDTO) {
+        projectService.removeMemberToProject(removeMemberDTO);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping("/{projectId}/updateProject")
+    public ResponseEntity<ProjectResponseDTO> updateDetailsToProject(@PathVariable UUID projectId, @RequestBody UpdateProjectDto updateProjectDTO) {
+        ProjectResponseDTO projectResponseDTO = projectService.updateProject(projectId, updateProjectDTO);
+        return new ResponseEntity<>(projectResponseDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{projectId}/delete")
+    public ResponseEntity<Void> deleteProject(@PathVariable UUID projectId) {
+        projectService.deleteProject(projectId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/{ownerId}/project")
+    public ResponseEntity<List<ProjectResponseDTO>> getProjectsByOwner(@PathVariable UUID ownerId) {
+        List<ProjectResponseDTO> projectResponseDTOS = projectService.getProjectsByOwner(ownerId);
+        return new ResponseEntity<>(projectResponseDTOS, HttpStatus.OK);
+    }
+
+    @GetMapping("/{projectId}/projectMembert")
+    public ResponseEntity<List<ProjectMemberDTO>> getProjectMembersByProject(@PathVariable UUID projectId) {
+        List<ProjectMemberDTO> list = projectService.getProjectMembers(projectId);
+        return new ResponseEntity<>(list, HttpStatus.OK);
+    }
+
+    @GetMapping("/{projectId}/get/{projectMemberId}")
+    public ResponseEntity<ProjectMemberDTO> getProjectMember(@PathVariable UUID projectId, @PathVariable UUID projectMemberId) {
+        ProjectMemberDTO projectMemberDTO = projectService.getProjectMember(projectId, projectMemberId);
+        return new ResponseEntity<>(projectMemberDTO, HttpStatus.OK);
+    }
+
+}
