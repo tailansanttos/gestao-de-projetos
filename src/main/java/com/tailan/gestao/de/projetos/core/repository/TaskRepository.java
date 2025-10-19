196d7d692df228d2554c55371d740c83888a272e
tailan <tailansanttos02@gmail.com>
2025-10-18 23:47:28 -0300

    feat: adicionado paginação no retorno de todas tasks de um projeto

--- src/main/java/com/tailan/gestao/de/projetos/core/repository/TaskRepository.java
+++ src/main/java/com/tailan/gestao/de/projetos/core/repository/TaskRepository.java
@@ -1,11 +1,20 @@
 package com.tailan.gestao.de.projetos.core.repository;
 
+import com.tailan.gestao.de.projetos.core.model.project.Project;
 import com.tailan.gestao.de.projetos.core.model.task.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+    Page<Task> findAllByProjectOrderByPriorityAsc(@Param("project") Project project, Pageable pageable);
+
 }
